67225a3ae959154f6c8c20a58b6bdfd24f4cceb0
Amy <dingxiaoyu168@outlook.com>
2025-02-16 20:47:22 +0800

    feat：新增菜品：文件上传

---
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,25 @@
+package com.sky.config;
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类，用于创建AliOssUtils对象
+ */
+@Configuration
+@Slf4j
+public class OssConfiguration {
+    @Bean //当项目启动时就会调用这个方法，把这个方法创建出来交给Spring容器管理
+    @ConditionalOnMissingBean //保证在整个Spring容器里只有一个Utils对象
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties) {
+        log.info("开始创建阿里云文件上传工具类对象：{}", aliOssProperties);
+        return new AliOssUtil(aliOssProperties.getEndpoint(),
+                aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),
+                aliOssProperties.getBucketName());
+    }
+}
