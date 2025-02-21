89e4d7791589aa2c50d1b392052a3a046916d6c3
Amy <dingxiaoyu168@outlook.com>
2025-02-21 10:39:04 +0800

    feat：查询当前登录用户的所有地址信息

---
+++ sky-server/src/main/java/com/sky/service/AddressBookService.java
@@ -0,0 +1,15 @@
+package com.sky.service;
+
+import com.sky.entity.AddressBook;
+import com.sky.result.Result;
+
+import java.util.List;
+
+public interface AddressBookService {
+     /**
+     * 查询当前登录用户的所有地址信息
+     * @param addressBook
+     * @return
+     */
+    List<AddressBook> list(AddressBook addressBook);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/AddressBookServiceImpl.java
@@ -0,0 +1,27 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.entity.AddressBook;
+import com.sky.mapper.AddressBookMapper;
+import com.sky.service.AddressBookService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class AddressBookServiceImpl implements AddressBookService {
+    @Autowired
+    private AddressBookMapper addressBookMapper;
+
+    /**
+     * 查询当前登录用户的所有地址信息
+     * @param addressBook
+     * @return
+     */
+    public List<AddressBook> list(AddressBook addressBook){
+        return addressBookMapper.list(addressBook);
+    }
+}
